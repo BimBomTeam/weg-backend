ef80c745d60a9da34f25dcc456df60603d574c95
K0L9 <kovalkola2@gmail.com>
2024-05-01 14:29:21 +0200

    Create serivce for communication with api gpt. Create functions for dialog

---
+++ WEG.Application/Claims/PromptsJsonContainer.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Text.Json.Serialization;
+
+namespace WEG.Application.Claims
+{
+    internal struct PromptsJsonContainer
+    {
+        /// <summary>
+        /// Format string. 
+        /// {0} - level
+        /// {1} - role
+        /// </summary>
+        [JsonPropertyName("generate-words-prompt")]
+        public string? GenerateWordsPrompt { get; set; }
+
+        /// <summary>
+        /// Format string. 
+        /// {0} - level
+        /// {1} - role
+        /// {2} - words list
+        /// 
+        /// </summary>
+        [JsonPropertyName("start-chat-prompt")]
+        public string StartChatPrompt { get; set; }
+
+        [JsonPropertyName("get-mistakes-prompt")]
+        public string GetMistakesPrompt { get; set; }
+    }
+}
--- WEG.WebUI/Controllers/AiCommunicationController.cs
+++ WEG.WebUI/Controllers/AiCommunicationController.cs
@@ -1,11 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WEG.Application.Services;
 using WEG.Infrastructure.Dto;
+using WEG.Infrastructure.Dto.Dialog;
 using WEG.Infrastructure.Models;
 using WEG.Infrastructure.Services;
 
 namespace WEG_Server.Controllers
-
 {
     [ApiController]
     [Route("api/[controller]")]
@@ -14,18 +14,17 @@ namespace WEG_Server.Controllers
         IAiCommunicationService aiCommunicationService;
         IAiService aiService;
 
-
         public AiCommunicationController(IAiCommunicationService aiCommunicationService, IAiService aiService)
         {
             this.aiCommunicationService = aiCommunicationService;
             this.aiService = aiService;
         }
 
-        [HttpPost(Name = "get-responseAI")]
+        [HttpPost("get-responseAI")]
         public async Task<IActionResult> GetFromAi([FromBody] DialogRequestDto request)
         {
             var response = await aiCommunicationService.GetMessageFromAi(request.Message);
             return Ok(response);
         }
-        }
+    }
 }
