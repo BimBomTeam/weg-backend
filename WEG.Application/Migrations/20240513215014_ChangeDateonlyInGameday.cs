cd4f2ea8fe59a4975bc6d8c7a8adbc1663ae96c9
K0L9 <kovalkola2@gmail.com>
2024-05-14 00:38:56 +0200

    Create migration. Update RedisCacheService

---
+++ WEG.Application/Migrations/20240513215014_ChangeDateonlyInGameday.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WEG.Application.Migrations
+{
+    public partial class ChangeDateonlyInGameday : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "Date",
+                table: "GameDays",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "GameDays",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+    }
+}
